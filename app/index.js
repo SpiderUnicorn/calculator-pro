c828c85f4ae2ba4686d753834a94a866cd8b140e
Christoffer Vestin <christoffer.vestin@tretton37.com>
2020-04-03 11:05:36 +0200

    Improvements

--- app/index.js
+++ app/index.js
@@ -1,11 +1,70 @@
-class Calculator {
+class CalculatorFactory {
+  static create(conf) {
+    return new Calculator(conf);
+  }
+}
+
+
+class CalculatorConfigurationColorBuilder {
+  resultColor(color) {
+    this.resultClr = color;
+    return this;
+  }
+
+  brownButtonColor(color) {
+    this.brownButtonColor = color;
+    return this;
+  }
+
+  Build() {
+    return {
+      submitButtonColor: this.submitButtonColor
+    }
+  }
+}
+
+class CalculatorConfiguration {
+  
+  constructor(initialValue, output, colors) {
+    this.initialValue = initialValue;
+    this.output = output;
+    this.colors = colors;
+  }
+  
+}
+
+class CalculatorConfigurationBuilder {
+
   constructor(initialValue) {
     this.initialValue = initialValue;
-    this.value = initialValue;
+  } 
+
+  output(node) {
+    this.output = node;
+    return this
+  }
+
+  addColors(colorBuilder) {
+    this.colors = colorBuilder.Build();
+    return this;
+  }
+
+
+  Build() {
+    return new CalculatorConfiguration(this.initialValue, this.output, this.colors);
+  }
+}
+
+class Calculator {
+  constructor(conf) {
+    this.output = conf.output;
+    this.initialValue = conf.initialValue;
+    this.value = conf.initialValue;
   }
 
   input(value) {
-    if (this.value == 0) this.value = value;
+    if (this.value == 0) 
+            this.value = value;
     this.value += value;
   }
 
@@ -18,19 +77,26 @@ class Calculator {
   }
 
   print() {
-    output.innerHTML = this.value;
+    this.output.innerHTML = this.value;
   }
 }
 
-var calculator = new Calculator(0);
-calculator.output = document.querySelector("#result");
+var cccb = new CalculatorConfigurationColorBuilder();
+cccb.resultColor("rgba(0, 0, 0, 0.8)").brownButtonColor = "orange";
+
+var c = new CalculatorConfigurationBuilder(0)
+  .output(document.querySelector("#result")).addColors(cccb)
+  .Build();
+
+var calculator = CalculatorFactory.create(c);
 
 
 document.addEventListener("click", function(event) {
   var text, action;
+
   text = event.target.innerText;
 
-  if (typeof text == "number") action = "inputNumber";
+  if (typeof -text == "number") action = "inputNumber";
   if (text == "AC") action = "reset";
   if (text == "=") action = "calculate";
 
